9336b20e879de0244f11aa64657555d905b0cadc
JASHVANTH-SR <128201154+JASHVANTH-SR@users.noreply.github.com>
2023-03-26 22:03:54 +0530

    Update hypot.py

--- hypot.py
+++ hypot.py
@@ -19,6 +19,8 @@ st.write("Alternative Hypothesis: Students who attend more lectures get better e
 
 # create a scatter plot
 
+st.markdown("## Scatter Plot")
+st.write("Used to visualize the relationship between attendance percentage and exam marks")
 st.markdown("### Scatter plot between Attendance and ESE")
 sns.scatterplot(x='Attendance', y='ESE', data=df)
 plt.title('Attendance vs End Semester Exam Marks')
@@ -38,7 +40,8 @@ st.pyplot()
 
 
 # create a bar chart
-st.markdown("### Bar Chart")
+st.markdown("## Bar Chart")
+st.write("Used to compare the average mid-semester exam marks and end semester exam marks based on attendance percentage")
 sns.barplot(x='Attendance', y='value', hue='variable', data=pd.melt(df_means, id_vars='Attendance', var_name='variable', value_name='value'))
 plt.title('Exam Marks vs Attendance')
 plt.xlabel('Attendance percentage')
@@ -54,22 +57,32 @@ low_attendance = df[df['Attendance'] < 80]['ESE']
 
 
 # create a box plot
-st.markdown("### Box Plot")
+st.markdown("## Box Plot")
+st.write("We can create a box plot to compare the distribution of exam marks for different attendance percentages. This will allow us to see the range of scores, the median, and any outliers for each group")
+st.markdown("### Attendance vs ESE")
 sns.boxplot(x='Attendance', y='ESE', data=df)
 plt.title('Attendance vs End Semester Exam Marks')
 plt.xlabel('Attendance percentage')
 plt.ylabel('End Semester Exam Marks')
 st.pyplot()
 
+st.markdown("### Attendance vs MSE")
+sns.boxplot(x='Attendance', y='MSE', data=df)
+plt.title('Attendance vs End Semester Exam Marks')
+plt.xlabel('Attendance percentage')
+plt.ylabel('End Semester Exam Marks')
+st.pyplot()
 
 # create a heatmap
-st.markdown("### Correlation Map")
+st.markdown("## Correlation Map")
+st.write("Used to visualize the correlation between attendance percentage, mid-semester exam marks, and end semester exam marks. This will allow us to see if there is a strong correlation between any of the variable")
 sns.heatmap(df.corr(), annot=True)
 plt.title('Correlation Heatmap')
 st.pyplot()
 
 # create a scatter plot matrix
-st.markdown("### Scatter-Pairplot")
+st.markdown("## Scatter-Pairplot")
+st.write("Used to compare the relationship between all the variables in the dataset. This will allow us to see if there is a strong correlation between any of the variables, and to identify any outliers.")
 sns.pairplot(df)
 plt.title('Scatter Plot Matrix')
 st.pyplot()
@@ -77,6 +90,8 @@ st.pyplot()
 
 
 # create a line chart
+st.markdown("## Lineplot")
+st.write("to visualize the trend of end semester exam marks as the attendance percentage increases. This will allow us to see if there is a linear relationship between the two variables")
 st.markdown("### Lineplot Attendance vs ESE")
 sns.lineplot(x='Attendance', y='ESE', data=df)
 plt.title('Attendance vs End Semester Exam Marks')
@@ -92,7 +107,8 @@ plt.ylabel('End Semester Exam Marks')
 st.pyplot()
 
 # create a histogram
-st.markdown("### Histogram")
+st.markdown("## Histogram")
+st.write("Used to visualize the distribution of attendance percentages in the dataset. This will allow us to see the frequency of different attendance percentages and identify any patterns or outliers")
 sns.histplot(data=df, x="Attendance", bins=10)
 plt.title('Distribution of Attendance Percentage')
 plt.xlabel('Attendance percentage')
@@ -100,6 +116,8 @@ plt.ylabel('Count')
 st.pyplot()
 
 # create a violin plot
+st.markdown("## Violin plot")
+st.write("We can create a violin plot to compare the distribution of end semester exam marks for different attendance percentages. This will allow us to see the shape of the distribution, and compare the spread and density of the distributions")
 st.markdown("### Violin plot Attendance vs ESE")
 sns.violinplot(x='Attendance', y='ESE', data=df)
 plt.title('Attendance vs End Semester Exam Marks')
@@ -116,14 +134,16 @@ st.pyplot()
 
 # create a bar chart
 
-st.markdown("### Violin plot Attendance vs ESE")
+st.markdown("## Barplot")
+st.write("to compare the mean end and mid semester exam marks for different attendance percentages. This will allow us to see if there is a significant difference in exam performance between students with different attendance percentages")
+st.markdown("### Bar plot Attendance vs ESE")
 sns.barplot(x='Attendance', y='ESE', data=df)
 plt.title('Attendance vs End Semester Exam Marks')
 plt.xlabel('Attendance percentage')
 plt.ylabel('Mean End Semester Exam Marks')
 st.pyplot()
 
-st.markdown("### Violin plot Attendance vs MSE")
+st.markdown("### Bar plot Attendance vs MSE")
 sns.barplot(x='Attendance', y='MSE', data=df)
 plt.title('Attendance vs Mid Semester Exam Marks')
 plt.xlabel('Attendance percentage')
@@ -131,6 +151,8 @@ plt.ylabel('Mean End Semester Exam Marks')
 st.pyplot()
 
 # create a joint plot
+st.markdown("## Joint Plot")
+st.write("to visualize the distribution of attendance percentage and end-mid semester exam marks together. This will allow us to see the relationship between the two variables and any patterns or outliers")
 st.markdown("### Joint plot Attendance vs ESE")
 sns.jointplot(x='Attendance', y='ESE', data=df)
 plt.title('Attendance vs End Semester Exam Marks')
